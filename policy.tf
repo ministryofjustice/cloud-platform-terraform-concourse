fd2d0942f8a4c0b60e4a1529539ae2ca39c43a50
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2021-05-17 18:01:44 +0100

    Add TagPolicy and TagOpenIDConnectProvider permissions to concourse user. This is recent change to EKS cluster and needed for creating cluster via concourse

--- policy.tf
+++ policy.tf
@@ -40,6 +40,8 @@ data "aws_iam_policy_document" "policy" {
       "iam:GetPolicyVersion",
       "iam:DeleteUserPermissionsBoundary",
       "iam:TagUser",
+      "iam:TagPolicy",
+      "iam:TagOpenIDConnectProvider",
     ]
 
     resources = [

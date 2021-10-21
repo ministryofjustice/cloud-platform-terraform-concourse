a75fdf19b9a49f261b4b8dbdaa5b9d3ac7619785
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-10-21 17:31:22 +0100

    fix test

--- example/main.tf
+++ example/main.tf
@@ -1,6 +1,11 @@
-
 terraform {
   backend "s3" {
+    bucket               = "cloud-platform-terraform-state"
+    region               = "eu-west-1"
+    key                  = "terraform.tfstate"
+    workspace_key_prefix = "aws-accounts/cloud-platform-aws/vpc/eks/components"
+    profile              = "moj-cp"
+    dynamodb_table       = "cloud-platform-terraform-state"
   }
 }
 

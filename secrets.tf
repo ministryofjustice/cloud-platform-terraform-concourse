ccf7394c12d65ef3acb5bf352ee56104837e7ffb
sj-williams <steve.williams@digital.justice.gov.uk>
2022-08-08 16:08:48 +0100

    refactor: edits to secret names

--- secrets.tf
+++ secrets.tf
@@ -1,10 +1,10 @@
 resource "aws_secretsmanager_secret" "concourse-test-dockerhub-username" {
-  name = "concourse/main/test_dockerhub_user"
+  name = "concourse/main/build-image-test/test_dockerhub_user"
   description = "concourse secrets test - dockerhub username"
 
 }
 
 resource "aws_secretsmanager_secret" "concourse-test-dockerhub-token" {
-  name = "concourse/main/test_dockerhub_token"
+  name = "concourse/main/build-image-test/test_dockerhub_token"
   description = "concourse secrets test - dockerhub token"
 }

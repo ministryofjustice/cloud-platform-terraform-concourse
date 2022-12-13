0e85ffff3b2ac3276b9dd9d02c552b69f5b3d358
jakemulley <jakemulley@users.noreply.github.com>
2022-12-13 16:57:30 +0000

    Remove unused local

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ locals {
   # This is the list of Route53 Hosted Zones in the DSD account that
   # cert-manager and external-dns will be given access to.
   live_workspace = "manager"
-  # rds_name       = var.is_prod ? "ci" : "${terraform.workspace}-concourse"
   live_domain    = "cloud-platform.service.justice.gov.uk"
 }
 

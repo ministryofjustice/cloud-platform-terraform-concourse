11deeab454ca28bdbba2ce6890de58682ff83edf
jakemulley <jakemulley@users.noreply.github.com>
2022-12-19 12:42:55 +0000

    Add IAM Access Analyzer permissions to global_account_policy

--- versions.tf
+++ versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = ">=4.24.0"
     }
     helm = {
@@ -9,19 +9,19 @@ terraform {
       version = ">=2.6.0"
     }
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
       version = ">=2.12.1"
     }
     kubectl = {
-      source = "gavinbunney/kubectl"
+      source  = "gavinbunney/kubectl"
       version = ">=1.13.2"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = ">=3.4.3"
     }
     tls = {
-      source = "hashicorp/tls"
+      source  = "hashicorp/tls"
       version = ">=4.0.3"
     }
   }

0f81b99a5ac83471cf5182f6d0bc9b4192f05537
jaskaransarkaria <jaskaran.sarkaria@digital.justice.gov.uk>
2025-06-24 16:14:47 +0100

    fixup! fixup! fix: 🐛 tf test

--- example/versions.tf
+++ example/versions.tf
@@ -1,12 +1,29 @@
-
 terraform {
-  required_version = ">= 1.2.5"
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = ">=4.24.0"
     }
     helm = {
-      source = "hashicorp/helm"
+      source  = "hashicorp/helm"
+      version = ">=2.6.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">=2.12.1"
+    }
+    kubectl = {
+      source  = "alekc/kubectl"
+      version = "2.1.3"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">=3.4.3"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">=4.0.3"
     }
   }
+  required_version = ">= 1.2.5"
 }

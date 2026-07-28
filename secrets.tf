953fbbb344301c770d8202def1e200f341ad79a9
Mike Bell <mike.bell1@justice.gov.uk>
2026-07-28 08:17:55 +0100

    feat: add new secret for dockerhub credentials

--- secrets.tf
+++ secrets.tf
@@ -149,3 +149,20 @@ resource "kubernetes_secret" "kraken_github_token" {
         value = var.kraken_github_token
   }
 }
+
+resource "kubernetes_secret" "chainguard_credentials" {
+  metadata {
+    name      = "dockerhub-image-pull-secret"
+    namespace = kubernetes_namespace.concourse_main.id
+  }
+
+  data = {
+    ".dockerconfigjson" = data.aws_ssm_parameter.dockerhub_registry_credentials.value
+  }
+
+  type = "kubernetes.io/dockerconfigjson"
+
+  depends_on = [
+    kubernetes_namespace.concourse_main.id
+  ]
+}
